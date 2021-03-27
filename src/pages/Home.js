d7052c75673bc97320c9a16bca1d45e3f9e5093e
Onaje Oladuwa <onaje.oladuwa@gmail.com>
2021-03-27 19:20:45 -0400

    Styled Header component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { OptForm } from "../components";
 import FaqsContainer from "../containers/Faqs";
 import JumbotronContainer from "../containers/Jumbotron";
 import FooterContainer from "../containers/Footer";
@@ -8,7 +9,15 @@ export default function Home() {
   return (
     <>
       <HeaderContainer>
-        <p>Hello</p>
+        <OptForm>
+          <OptForm.Input placeholder="Email Address" />
+          <OptForm.Button>Try it now</OptForm.Button>
+          <OptForm.Break />
+          <OptForm.Text>
+            Ready to watch? Enter your email to create or restart your
+            membership
+          </OptForm.Text>
+        </OptForm>
       </HeaderContainer>
 
       <JumbotronContainer />
