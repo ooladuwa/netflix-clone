4cd750c6569bd34813944d602a13e5e3c3fe3b08
Onaje Oladuwa <onaje.oladuwa@gmail.com>
2021-03-27 13:27:15 -0400

    Forgot to connect to Github and lost my first four commits. Thus far I have built and styled the jumbotron, and footer components. React Router is connected and routes ready. I will regularly commit each feature now.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { GlobalStyles } from "./global-styles"
+import { App } from './app';
+
+ReactDOM.render(
+<>  
+  <GlobalStyles />
+    <App />
+</>, 
+  document.getElementById('root'));
