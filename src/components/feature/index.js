564be29a0daed3a6b8f549908b373341be3b6b02
Onaje Oladuwa <onaje.oladuwa@gmail.com>
2021-03-27 19:52:24 -0400

    Created Feature component

---
+++ src/components/feature/index.js
@@ -0,0 +1,17 @@
+// compound components to put the component together
+// add the feature component to index.js
+
+import React from "react";
+import { Container, Title, SubTitle } from "./styles/Feature";
+
+export default function Feature({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>;
+}
+
+Feature.Title = function FeatureTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
+
+Feature.SubTitle = function FeatureSubTitle({ children, ...restProps }) {
+  return <SubTitle {...restProps}>{children}</SubTitle>;
+};
