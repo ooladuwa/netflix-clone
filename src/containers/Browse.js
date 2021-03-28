de6683a6dfb6290d69aa37fa3f3b3fb69903fb3b
Onaje Oladuwa <32712596+ooladuwa@users.noreply.github.com>
2021-03-28 02:30:37 -0400

    Revert "Profile"

--- src/containers/Browse.js
+++ src/containers/Browse.js
@@ -1,24 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import { Header } from "../components";
 import * as ROUTES from "../constants/Routes";
 import { FirebaseContext } from "../context/Firebase";
 import { SelectProfileContainer } from "./Profiles";
 import FooterContainer from "./Footer";
+import { profile } from "react";
 
 export function BrowseContainer() {
-  const [profile, setProfile] = useState({});
-
   const user = {
     displayName: "Ralph",
     photoURL: "1",
   };
-
   return profile.displayName ? (
     <>
       <p>Browse Container</p>
       <FooterContainer />
     </>
   ) : (
-    <SelectProfileContainer user={user} setProfile={setProfile} />
+    <SelectProfileContainer />
   );
 }
