883f496e7dd8f18a389237c39e2785c3e26d7cc0
Onaje Oladuwa <onaje.oladuwa@gmail.com>
2021-03-27 22:30:01 -0400

    Styled Form component and SignIn page

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -4,11 +4,43 @@ import { HeaderContainer } from "../containers/Header";
 
 export default function SignIn() {
   const [error, setError] = useState("");
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignIn = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <HeaderContainer>
       <Form>
         <Form.Title>Sign In</Form.Title>
         {error && <Form.Error>{error}</Form.Error>}
+
+        <Form.Base onSubmit={handleSignIn} method="POST">
+          <Form.Input
+            placeholder="Email address"
+            value={emailAddress}
+            onChange={({ target }) => setEmailAddress(target.value)}
+          />
+          <Form.Input
+            type="password"
+            value="password"
+            autoComplete="off"
+            placeholder="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+          <Form.Submit disabled={false} type="submit">
+            Sign In
+          </Form.Submit>
+
+          <Form.Text>
+            New to NetFlix? <Form.Link to="/signup">Sign up now.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA.
+          </Form.TextSmall>
+        </Form.Base>
       </Form>
     </HeaderContainer>
   );
